3f467b3939a733293e697078ddabaaec198e9a0d
syiin <xbio.oo@gmail.com>
2019-07-11 16:57:53 +0800

    splitVec() now returns tuple of datavec

--- main.cpp
+++ main.cpp
@@ -4,13 +4,15 @@
 #include <map>
 #include <array>
 #include <functional>
+#include <tuple>
 
 enum terrain { HILLY, FLAT };
 typedef std::array <float, 9> datarow;
 typedef std::vector <datarow> datavec;
+typedef std::tuple<datavec, datavec> datavectup;
 
 float 
-calcSingleEntropy(datavec dataVec,
+calcClassEntropy(datavec dataVec,
     float klassIdx,
     float klass)
 {
@@ -24,18 +26,21 @@ calcSingleEntropy(datavec dataVec,
   return -(p) * log2(p); 
 }
 
-datavec
+std::tuple<datavec, datavec> 
 splitVec(datavec dataVector, 
     unsigned int paramIdx,
     std::function<bool (float)> testFunc)
 {  
-  datavec outputVec;
+  datavec outputVec1;
+  datavec outputVec2;
   for (auto const& dataRow: dataVector){
     if (testFunc(dataRow[paramIdx])){
-      outputVec.push_back(dataRow);
+      outputVec1.push_back(dataRow);
+    } else {
+      outputVec2.push_back(dataRow);
     }
   }
-  return outputVec;
+  return std::make_tuple(outputVec1, outputVec2); 
 }
 
 
@@ -63,8 +68,9 @@ main()
   testVec.push_back(testRow2);
   testVec.push_back(testRow3);
   
-  datavec testoVec;
-  testoVec = splitVec(testVec, 0, gtY(0));
-  float S = calcSingleEntropy(testVec, 1, 1);
-  S = calcSingleEntropy(testVec, 0, 1);
+  datavec testoVec1;
+  datavec testoVec2;
+  tie(testoVec1, testoVec2) = splitVec(testVec, 0, gtY(0));
+  float S = calcClassEntropy(testVec, 1, 1);
+  S = calcClassEntropy(testVec, 0, 1);
 }
